9a286a1641bbd4dc138d24f570435cb037304c06
Nicolás Alliaume <nicolas@on-lab.com>
2017-09-26 17:15:02 -0400

    added email integration version 1

--- routes/email/routes.js
+++ routes/email/routes.js
@@ -1,6 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const superagent = require('superagent');
+const log = require('./../../utils/logger');
+const Endpoints = require('./../../conf/services-endpoints');
+const TempConfig = require('./../../conf/tmp');
+let multer = require('multer');
+let upload = multer();
 
-router.get('/', function(req, res) { res.sendStatus(501) });
+router.get('/', function(res,res) { res.sendStatus(200); });
+router.post('/newemail', upload.any(), emailReceived);
 
 module.exports = router;
+
+
+function emailReceived(req, res) {
+	log('info', 'email-newemail', JSON.stringify(req.body));
+
+	const { fromEmail, subject, body } = JSON.parse(req.body.data);
+	
+	const newTask = {
+		title : subject, 
+		description : body,
+		tags : ['imported'],
+		project : TempConfig.importTestProject, // TODO map from board? same name?
+		created_by : TempConfig.importTestUser, // need to map creator by trello username. we should do this on the services project
+		origin : 'email'
+	}
+
+	superagent
+		.post(Endpoints.addTask)
+		.send(newTask)
+		.set('Authorization', Endpoints.authToken)
+		.then(response => response.body)
+		.then(body => log('info', 'email-newemail-response', JSON.stringify(body)))
+		.catch(error => log('error', 'email-newemail-response', error.message))
+
+	res.sendStatus(200);
+}
