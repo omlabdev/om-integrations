610cc2830ca76ead134eddee9864005a6cd4dae8
Nicolás Alliaume <nicolas@on-lab.com>
2017-10-03 03:04:11 -0400

    Building and pushing to heroku

--- routes/git/routes.js
+++ routes/git/routes.js
@@ -64,7 +64,7 @@ function sendSlackMessageOfferingWorkEntry(slackId, gitRepo, summary) {
 				color : getRandomColor()
 			},
 			{
-				title : 'Push summary'
+				title : 'Push summary',
 				text : summary,
 				color : getRandomColor(),
 				mrkdwn_in: ['text']
