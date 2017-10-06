95e6c2d4a63405d9dfc1e10da5ec3e0270184ff3
Nicolás Alliaume <nicolas@on-lab.com>
2017-10-05 20:36:57 -0400

    Building and pushing to heroku

--- routes/slack/time_options.js
+++ routes/slack/time_options.js
@@ -3,7 +3,7 @@ const hourOptions = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5
 const options = hourOptions.map(o => { 
 	const seconds = o * 3600;
 	const formattedHour = String(Math.floor(o) + 100).substring(1);
-	const formattedMinutes = String((o - Math.floor(o)) * 60);
+	const formattedMinutes = String(((o - Math.floor(o)) * 60) + 100).substring(1);
 	const formatted = formattedHour + ':' + formattedMinutes;
 	return { text: formatted, value: seconds }
 })
