9e80cd2a3e01614e34e63fdb2dc21701aedbead5
Nicolás Alliaume <nicolas@on-lab.com>
2017-10-10 12:28:24 -0400

    changed TW event to TASK.UPDATED to see what data is sent by TW

--- routes/teamwork/routes.js
+++ routes/teamwork/routes.js
@@ -10,7 +10,54 @@ router.post('/webhook/:integrationId', webhook);
 
 module.exports = router;
 
+function webhook(req, res) {
+	log('info', 'teamwork-webhook', JSON.stringify(req.body));
 
+	// just log for now
+	// changed event to TASK.UPDATED
+
+	// const { id, name, priority, status, tags, projectId, description } = req.body.task;
+	// const listName = req.body.taskList.name;
+
+	// if (status !== "new") {
+	// 	log('info', 'teamwork-webhook-status', 'Status is NOT new. Status = ' + status + '. FINISHING HERE');
+	// 	return;
+	// }
+
+	// const integrationId = req.params.integrationId;
+	// const auth = Endpoints.authToken();
+	// getIntegrationWithId(integrationId, auth, (error, integration) => {
+	// 	if (error) {
+	// 		return log('error', 'teamwork-get-integration-response', JSON.stringify(error));
+	// 	}
+
+	// 	// get project _id
+	// 	const mappingKey = projectId.toString();
+	// 	const project = integration.mappings[mappingKey];
+	// 	if (!project) {
+	// 		return log('error', 'teamwork-integration-mapping-missing', 'Mapping for project ' + mappingKey + ' does not exist on integration ' + integration._id);
+	// 	}
+
+	// 	const site = `https://${integration.meta.account}.teamwork.com`;
+	// 	const link = site + '/index.cfm#tasks/' + id;
+	// 	const newTask = {
+	// 		title : name, 
+	// 		description : description ? description : '',
+	// 		tags : tags.concat(integration.auto_tags || []),
+	// 		project,
+	// 		origin : 'teamwork',
+	// 		external_url : link,
+	// 		external_id : id
+	// 	}
+
+	// 	sendNewTask(newTask);
+	// })
+	
+	// respond immediately
+	res.sendStatus(200);
+}
+
+/*
 function webhook(req, res) {
 	log('info', 'teamwork-webhook', JSON.stringify(req.body));
 
@@ -54,6 +101,7 @@ function webhook(req, res) {
 	// respond immediately
 	res.sendStatus(200);
 }
+*/
 
 function sendNewTask(task) {
 	superagent
