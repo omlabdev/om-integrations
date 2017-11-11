162381d3b5e9195274af416ff11f564d31314025
Nicolás Alliaume <nicolas@on-lab.com>
2017-11-10 23:08:58 -0300

    Building and pushing to heroku

--- routes/teamwork/routes.js
+++ routes/teamwork/routes.js
@@ -20,13 +20,20 @@ module.exports = router;
 function taskCreated(req, res) {
 	log('info', 'teamwork-webhook-task-create', JSON.stringify(req.body));
 
-	const task = getTaskFromRequestBody(req.body);
 	const integrationId = req.params.integrationId;
 
 	// respond immediately
 	res.sendStatus(200);
-	
-	onTaskCreatedOrUpdated(task, integrationId);
+
+	const auth = Endpoints.authToken();
+	getIntegrationWithId(integrationId, auth, (error, integration) => {
+		if (error) return log('error', 'teamwork-get-integration-response', JSON.stringify(error));
+
+		const task = getTaskFromRequestBody(req.body, integration);
+		if (!task) return;
+
+		onTaskCreatedOrUpdated(task, integration);
+	}
 
 
 	// const integrationId = req.params.integrationId;
@@ -62,12 +69,21 @@ function taskCreated(req, res) {
 	
 }
 
-function getTaskFromRequestBody(body) {
-	const { id, name, priority, status, tags, projectId, description } = req.body.task;
-	const listName = req.body.taskList.name;
+function getTaskFromRequestBody(body, integration) {
+	const { id, name, priority, status, tags, projectId, description } = body.task;
+	const listName = body.taskList.name;
 
 	const site = `https://${integration.meta.account}.teamwork.com`;
 	const link = site + '/index.cfm#tasks/' + id;
+
+	const mappingKey = projectId.toString();
+	const project = integration.mappings[mappingKey];
+
+	if (!project) {
+		log('error', 'teamwork-integration-mapping-missing', 'Mapping for project ' 
+			+ mappingKey + ' does not exist on integration ' + integration._id);
+		return null;
+	}
 	
 	return {
 		title : name, 
@@ -76,7 +92,7 @@ function getTaskFromRequestBody(body) {
 		origin : 'teamwork',
 		external_url : link,
 		external_id : id,
-		project : projectId // teamwork project id
+		project : project
 	}
 }
 
@@ -85,40 +101,22 @@ function taskUpdated(req, res) {
 	res.sendStatus(200);
 }
 
-function onTaskCreatedOrUpdated(taskData, integrationId) {
-	const auth = Endpoints.authToken();
-	getIntegrationWithId(integrationId, auth, (error, integration) => {
-		if (error) {
-			return log('error', 'teamwork-get-integration-response', JSON.stringify(error));
+function onTaskCreatedOrUpdated(taskData, integration) {
+	// fetch the task from TW to check whether is complete.
+	// if not, check if it is assigned to someone we care
+	fetchTaskFromTeamwork(taskData.id, integration, (error, task) => {
+		log('info', 'teamwork-fetch-task-response', JSON.stringify(task));
+
+		if (task.completed)
+			return log('info', 'teamwork-fetch-task-response', 'Task is already completed');
+		
+		const assigned = task['responsible-party-ids'] !== undefined;
+		if (!assigned) {
+			return log('info', 'teamwork-fetch-task-response', 'Task is not assigned to anyone');
+		} else {
+			log('info', 'teamwork-fetch-task-response', 'Task will be created');
+			sendNewTask(taskData);
 		}
-
-		// check if the project the task is associated is configured
-		// for this integration
-		const mappingKey = taskData.project.toString();
-		const project = integration.mappings[mappingKey];
-		if (!project) {
-			return log('error', 'teamwork-integration-mapping-missing', 'Mapping for project ' + mappingKey + ' does not exist on integration ' + integration._id);
-		}
-
-		// replace TW project id with OM's project id
-		taskData.project = project;
-
-		// fetch the task from TW to check whether is complete.
-		// if not, check if it is assigned to someone we care
-		fetchTaskFromTeamwork(taskData.id, integration, (error, task) => {
-			log('info', 'teamwork-fetch-task-response', JSON.stringify(task));
-
-			if (task.completed)
-				return log('info', 'teamwork-fetch-task-response', 'Task is already completed');
-			
-			const assigned = task['responsible-party-ids'] !== undefined;
-			if (!assigned) {
-				return log('info', 'teamwork-fetch-task-response', 'Task is not assigned to anyone');
-			} else {
-				log('info', 'teamwork-fetch-task-response', 'Task will be created');
-				sendNewTask(taskData);
-			}
-		})
 	})
 }
 
