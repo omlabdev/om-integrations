1df157792d09a89a79fbaae44a3e478c6aeea50a
Nicolás Alliaume <nicolas@on-lab.com>
2018-08-10 10:02:52 -0400

    changed confs to .env and process.ENV

--- conf/tokens.js
+++ conf/tokens.js
@@ -4,20 +4,20 @@ module.exports = {
 
 	SLACK_APP_TOKEN	: process.env.SLACK_APP_TOKEN, // This is a slack app with a bot
 
-	SLACK_BOT_TOKEN : 'xoxb-274781105765-S68pbBTi0zYSp3NEJ2COu7SX', // This is a bot within the app
+	SLACK_BOT_TOKEN : process.env.SLACK_BOT_TOKEN, // This is a bot within the app
 
-	SLACK_CLIENT_ID	: '3502577971.249605696933',
+	SLACK_CLIENT_ID	: process.env.SLACK_CLIENT_ID,
 
-	SLACK_CLIENT_SECRET : '398e0c770209048339805ef5718d3d83',
+	SLACK_CLIENT_SECRET : process.env.SLACK_CLIENT_SECRET,
 
 	/* tokens used to authenticate to OM */
 
-	SLACK_TOKEN 	: 'biTlZ0Ica2fRNA4NFYLAWK33',
+	SLACK_TOKEN 	: process.env.SLACK_TOKEN,
 
-	GIT_TOKEN 		: 'lkjLKNLKKNKABUHIUHS767823',
+	GIT_TOKEN 		: process.env.GIT_TOKEN,
 
-	TRELLO_TOKEN 	: 'lkjLKNLKKNKABUHIUHS767824',
+	TRELLO_TOKEN 	: process.env.TRELLO_TOKEN,
 
-	EMAIL_TOKEN		: '9078tyausgdhjkn89b38998iuyKHJGYU897' // just an ugly random string
+	EMAIL_TOKEN		: process.env.EMAIL_TOKEN // just an ugly random string
 
 }
